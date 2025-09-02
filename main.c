db86e7ef30e0147ab5e11072be516a6dd8579b63
Иван <92312258+Lekerest@users.noreply.github.com>
2025-09-02 09:38:50 +0700

    Update

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 // Функция: выводит приветственное сообщение
 void PrintHello() {
-    printf("Hello World\n");
+    printf("Hello World!\n");
 }
 
 // Функция: ввод двух чисел по отдельности, проверка и сумма
@@ -13,23 +13,23 @@ void CalculateSum() {
 
     // Ввод A
     do {
-        printf("Введите число A (>= 0): ");
+        printf("Введите число A (0 <= A <= 100): ");
         scanf("%d", &numberA);
 
-        if (numberA < 0) {
-            printf("Ошибка: A должно быть больше или равно 0\n");
+        if (numberA < 0 || numberA > 100) {
+            printf("Ошибка: A должно быть в пределах от 0 до 100\n");
         }
-    } while (numberA < 0);
+    } while (numberA < 0 || numberA > 100);
 
     // Ввод B
     do {
-        printf("Введите число B (<= 100): ");
+        printf("Введите число B (0 <= B <= 100): ");
         scanf("%d", &numberB);
 
-        if (numberB > 100) {
-            printf("Ошибка: B должно быть меньше или равно 100\n");
+        if (numberB < 0 || numberB > 100) {
+            printf("Ошибка: B должно быть в пределах от 0 до 100\n");
         }
-    } while (numberB > 100);
+    } while (numberB < 0 || numberB > 100);
 
     int sum = numberA + numberB;
     printf("Сумма: %d\n", sum);
@@ -40,100 +40,94 @@ void CalculateOperationsMultiplicationRemainder() {
     int numberA, numberB;
 
     do {
-        printf("Введите два числа A и B (A >= 0, B <= 100) через пробел: ");
+        printf("Введите два числа A и B (0 <= A <= 100, 0 <= B <= 100) через пробел: ");
         int inputResult = scanf("%d %d", &numberA, &numberB);
 
         if (inputResult != 2) {
             printf("Ошибка: нужно ввести два числа\n");
-
-            // Очистка буфера
             while (getchar() != '\n');
             continue;
         }
 
-        if (numberA < 0) {
-            printf("Ошибка: A должно быть больше или равно 0\n");
+        if (numberA < 0 || numberA > 100) {
+            printf("Ошибка: A должно быть в пределах от 0 до 100\n");
         }
-        if (numberB > 100) {
-            printf("Ошибка: B должно быть меньше или равно 100\n");
+        if (numberB < 0 || numberB > 100) {
+            printf("Ошибка: B должно быть в пределах от 0 до 100\n");
         }
-    } while (numberA < 0 || numberB > 100);
+    } while (numberA < 0 || numberA > 100 || numberB < 0 || numberB > 100);
 
     int multiplication = numberA * numberB;
     printf("Произведение: %d\n", multiplication);
 
-    int remainder = numberA % numberB;
-    printf("Остаток от деления %d на %d: %d\n", numberA, numberB, remainder);
+    if (numberB != 0) {
+        int remainder = numberA % numberB;
+        printf("Остаток от деления %d на %d: %d\n", numberA, numberB, remainder);
+    } else {
+        printf("Ошибка: деление на ноль невозможно\n");
+    }
 }
 
 void CalculateOperationsDivisionAverage() {
     int numberA, numberB;
 
     do {
-        printf("Введите два числа A и B (A >= 0, B <= 100) через пробел: ");
+        printf("Введите два числа A и B (0 <= A <= 100, 0 <= B <= 100) через пробел: ");
         int inputResult = scanf("%d %d", &numberA, &numberB);
 
         if (inputResult != 2) {
             printf("Ошибка: нужно ввести два числа\n");
-
-            // Очистка буфера
             while (getchar() != '\n');
             continue;
         }
 
-        if (numberA < 0) {
-            printf("Ошибка: A должно быть больше или равно 0\n");
+        if (numberA < 0 || numberA > 100) {
+            printf("Ошибка: A должно быть в пределах от 0 до 100\n");
         }
-        if (numberB > 100) {
-            printf("Ошибка: B должно быть меньше или равно 100\n");
+        if (numberB < 0 || numberB > 100) {
+            printf("Ошибка: B должно быть в пределах от 0 до 100\n");
         }
-    } while (numberA < 0 || numberB > 100);
+    } while (numberA < 0 || numberA > 100 || numberB < 0 || numberB > 100);
 
-    int division = numberA / numberB;
-    printf("Деление: %d\n", division);
+    if (numberB != 0) {
+        int division = numberA / numberB;
+        printf("Деление: %d\n", division);
+    } else {
+        printf("Ошибка: деление на ноль невозможно\n");
+    }
 
     float averageAB = (float)(numberA + numberB) / 2;
     printf("Среднее арифметическое %d c %d: %.2f\n", numberA, numberB, averageAB);
 }
 
-void SummaNaturalNumbers() 
-{
+void SummaNaturalNumbers() {
     int numberA;
     int SummaNumbers = 0;
      
-    do 
-    {
-        printf("Введите число A (A > 0): ");
+    do {
+        printf("Введите число A (0 <= A <= 100): ");
         int inputResult = scanf("%d", &numberA);
 
-        if (inputResult != 1) 
-        {
+        if (inputResult != 1) {
             printf("Ошибка: нужно ввести одно число\n");
-
-            // Очистка буфера
             while (getchar() != '\n');
             continue;
         }
-        if (numberA <= 0) 
-        {
-            printf("Ошибка: A должно быть больше 0\n");
+        if (numberA < 0 || numberA > 100) {
+            printf("Ошибка: A должно быть в пределах от 0 до 100\n");
         }
-    } while (numberA <= 0);
+    } while (numberA < 0 || numberA > 100);
 
-    for (int i = 1; i <= numberA; i++)
-    {
-        SummaNumbers += i;
-    }
+    SummaNumbers = (numberA * (numberA + 1)) / 2;
     printf("Сумма натуральных чисел последовательности 1 -> %d : %d\n", numberA, SummaNumbers);
-
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    /*PrintHello();
+    PrintHello();
     CalculateSum();
     CalculateOperationsMultiplicationRemainder();
-    CalculateOperationsDivisionAverage();*/
+    CalculateOperationsDivisionAverage();
     SummaNaturalNumbers();
 
     return 0;
