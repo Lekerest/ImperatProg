fd027cfcf199e7f3b7cd17743117d3b0772d0fef
Lekerest <ikhritankov@yandex.ru>
2025-09-10 22:34:37 +0700

    3

---
+++ 2_pac_tasks/3_tasks/3_task.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() 
+{
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+    
+    if (input == NULL || output == NULL) 
+    {
+        return 1;
+    }
+
+    int count_number;
+    if (fscanf(input, "%d", &count_number) != 1) {
+        fprintf(stderr, "Ошибка чтения количества чисел\n");
+        fclose(input);
+        fclose(output);
+        return 1;
+    }
+
+    int count_zero = 0;
+    int count_positive = 0;
+    int count_negative = 0;
+
+    for (int i = 0; i < count_number; i++)
+    {
+        int a;
+        if (fscanf(input, "%d", &a) != 1) {
+            fprintf(stderr, "Ошибка чтения числа на позиции %d\n", i + 1);
+            fclose(input);
+            fclose(output);
+            return 1;
+        }
+
+        if (a > 0) count_positive++;
+        else if (a < 0) count_negative++;
+        else count_zero++;
+    }
+
+    double positive = (double)count_positive / count_number;
+    double negative = (double)count_negative / count_number;
+    double zero = (double)count_zero / count_number;
+
+    fprintf(output, "%0.5lf %0.5lf %0.5lf",
+            ((long)(negative * 100000)) / 100000.0,
+            ((long)(zero * 100000)) / 100000.0,
+            ((long)(positive * 100000)) / 100000.0);
+
+
+    fclose(input);
+    fclose(output);
+    return 0;
+}
