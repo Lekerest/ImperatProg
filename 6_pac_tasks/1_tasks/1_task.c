055d4eeda15a8bebb4655b55f3563cbdb6ffc018
Lekerest <92312258+Lekerest@users.noreply.github.com>
2025-10-20 20:53:17 +0700

    Create 1_task.c

---
+++ 6_pac_tasks/1_tasks/1_task.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+// функция определения порядка байт в системе
+int is_little_endian(void)
+{
+    unsigned short test = 0x1;
+    return *((unsigned char *)&test) == 0x1;
+}
+
+// функция перестановки байт
+unsigned int swap_endian(unsigned int x)
+{
+    return ((x >> 24) & 0x000000FF) |
+           ((x >> 8)  & 0x0000FF00) |
+           ((x << 8)  & 0x00FF0000) |
+           ((x << 24) & 0xFF000000);
+}
+
+int main(void)
+{
+    FILE *input = fopen("input.txt", "rb");
+    FILE *output = fopen("output.txt", "wb");
+
+    if (!input || !output)
+    {
+        return 1;
+    }
+
+    unsigned int N;
+    fread(&N, 4, 1, input);
+
+    unsigned int N_swapped = swap_endian(N);
+    int file_little_endian;
+
+    if (N >= 1 && N <= 10000)
+    {
+        file_little_endian = is_little_endian();
+    }
+    else if (N_swapped >= 1 && N_swapped <= 10000)
+    {
+        N = N_swapped;
+        file_little_endian = !is_little_endian();
+    }
+    else
+    {
+        fclose(input);
+        fclose(output);
+        return 1;
+    }
+
+    int sum = 0;
+
+    for (unsigned int i = 0; i < N; i++)
+    {
+        int num;
+        fread(&num, 4, 1, input);
+
+        if (file_little_endian != is_little_endian())
+        {
+            num = (int)swap_endian((unsigned int)num);
+        }
+
+        sum += num;
+    }
+
+    unsigned int result = (unsigned int)sum;
+
+    if (file_little_endian != is_little_endian())
+    {
+        result = swap_endian(result);
+    }
+
+    fwrite(&result, 4, 1, output);
+
+    fclose(input);
+    fclose(output);
+
+    return 0;
+}
